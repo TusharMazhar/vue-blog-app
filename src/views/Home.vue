18ac46b65154d79b1ca8e12c97f6b0c1c9afd52e
TusharMazharNsu <tusharm@inneed.cloud>
2021-03-15 11:37:49 -0700

    initial setup complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+       All blog List
+
+       <div>
+         <router-link to="/about">Create Blog</router-link>
+       </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
