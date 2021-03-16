1b7a61f1f9d827844e3e948158c8cde7d1ce0bec
TusharMazharNsu <tusharm@inneed.cloud>
2021-03-16 04:09:18 -0700

    format all source code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="home">
-       <v-container>
-
-          <div style="margin:10px 10px;">
-            <router-link to="/create-blog" style="text-decoration:none"><v-btn color="pink white--text" > Create New Blog</v-btn></router-link>
-          </div>
-
-       </v-container>
+    
+    <v-container>
+      <div style="margin:10px 10px;">
+        <router-link to="/create-blog" style="text-decoration:none"><v-btn color="pink white--text" > Create New Blog</v-btn></router-link>
+      </div>
+    </v-container>
      
-       <v-container>
-          <div style="margin-bottom:10px">
-                <h1 style="text-align:center" v-if="blogs.length!=0">All Blogs</h1>
-          </div>
-          
-          <v-row v-if="blogs.length>0">
+    <v-container>
+      <div style="margin-bottom:10px">
+        <h1 style="text-align:center" v-if="blogs.length!=0">All Blogs</h1>
+      </div>
+      
+      <v-row v-if="blogs.length>0">
 
-            <v-col  v-for="blog in blogs" :key="blog.id">
-            <v-card
-            class="mx-auto"
-            max-width="200"
-          >
+        <v-col  v-for="blog in blogs" :key="blog.id">
+          <v-card
+          class="mx-auto"
+          max-width="200"
+        >
             <v-img
               class="white--text align-end"
               height="200px"
@@ -31,20 +30,17 @@
             <v-card-text class="text--primary">
               <div>{{blog.description}}</div>
             </v-card-text>
-        
           </v-card>
+        </v-col>
+        
+      </v-row> 
+      <v-row v-else style="margin-top:10px">
+        <div style="margin:auto">
+          <p style="font-size:30px;font-weight:bold;color:red;text-align:center">No Blog Created</p>
+        </div>  
+      </v-row> 
+    </v-container>
 
-            </v-col>
-            
-          </v-row> 
-          <v-row v-else style="margin-top:10px">
-            <div style="margin:auto">
-                <p style="font-size:30px;font-weight:bold;color:red;text-align:center">No Blog Created</p>
-            </div>
-             
-          </v-row> 
-          
-       </v-container>
   </div>
 </template>
 
@@ -55,12 +51,11 @@ export default {
   name: 'Home',
   data(){
     return{
+
       blogs: this.$store.state.blogs
           
     }
   },
-  created(){
-   
-  }
+
 }
 </script>
