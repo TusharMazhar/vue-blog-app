c1c167c9e50c8876bb8a015bdfe7047a33bd8e81
TusharMazharNsu <tusharm@inneed.cloud>
2021-03-16 22:18:00 -0700

    image salect and show in preview page complete

--- src/views/CreateBlog.vue
+++ src/views/CreateBlog.vue
@@ -1,26 +1,21 @@
 <template>
   <div>
-
     <div style="margin-top:20px">
       <p style="text-align:center;font-size:25px;color:green;font-weight:bold"> Create New Blog </p>
     </div>
-
     <v-card class="mx-auto my-12" max-width="600">
       <v-snackbar v-model="snackbar" :timeout="timeout"  multi-line>
         {{ text }}
-
         <template v-slot:action="{ attrs }">
           <v-btn v-bind="attrs" @click="snackbar = false" color="success" text>
             Close
           </v-btn>
         </template>
       </v-snackbar>
-
       <div>
         <v-card-title class="justify-center" v-if="step<3">Step-{{step}} </v-card-title>
         <v-card-title class="justify-center" v-else style="font-size:25px;;color:red"> Preview  </v-card-title>
       </div>
-
       <div class="mx-6">
         <v-text-field
           v-if="step===1"
@@ -31,7 +26,6 @@
           :rules="titleRules"
           placeholder="Title"
         ></v-text-field>
-
         <v-textarea 
           v-model="description" 
           outlined placeholder="Description" 
@@ -39,7 +33,6 @@
           :rules="descriptionRules" 
           v-if="step===1">
         </v-textarea>
-
         <v-file-input
           v-model="image1"
           v-if="step===2"
@@ -49,6 +42,7 @@
           :rules="imageRules"
           ref="field"
           prepend-icon="mdi-camera"
+          @change="onAddFiles"
         >
         </v-file-input> 
         <v-file-input
@@ -59,6 +53,7 @@
           multiple
           :rules="imageRules"
           ref="field"
+          @change="onAddFiles2"
           prepend-icon="mdi-camera"
         >
         </v-file-input> 
@@ -101,26 +96,27 @@
           max-width="344"
           class="mx-auto"
         > 
-
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title class="headline" style="color:green">{{title}}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-        
           <img
-            v-for="img in inputImages" :key="img.index"
+            v-for="src in previews" :key="src.index"
             style="margin-left:10px"
-            :src='img'
+            :src="src"
             height="100"
           />
-        
+          <img
+            v-for="src in previews2" :key="src.index"
+            style="margin-left:10px"
+            :src="src"
+            height="100"
+          />
           <v-card-text>
             {{description}}
           </v-card-text>
-      
         </v-card>
-
         <div style="text-align:center">
           <v-btn @click="prev"  class="mb-5" v-if="step>1 && step<4" style="margin-right:10px">
             Prev
@@ -139,11 +135,13 @@
     </v-card>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
+      previews: [],
+      previews2: [],
+      // errorImage: "url of an image to use to indicate an error",
       titleRules:[ v => v.length>0 || 'Min 1 Charcter'],
       descriptionRules: [v => v.length >100 || 'Min 100 characters'],
       imageRules:[() =>this.image1.length>0   || 'Minimum 1  or Maximum 5 image1 You will be able to select'],
@@ -164,6 +162,22 @@ export default {
     }
   },
   methods:{
+    onAddFiles(files) {
+    this.previews = [];
+    files.forEach((file, index) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', e => this.previews[index] = e.target.result);
+        reader.readAsDataURL(file);
+      })
+    },
+     onAddFiles2(files) {
+    this.previews2 = [];
+    files.forEach((file, index) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', e => this.previews2[index] = e.target.result);
+        reader.readAsDataURL(file);
+      })
+    },
     next(){
       this.$refs.field.focus()
       if(this.$refs.field.validate()){
@@ -172,8 +186,8 @@ export default {
     },
     next2(){
       this.inputImages=[this.image1,this.image2,this.image3,this.image4,this.image5]
-      this.uploadImage = this.image1[0].name || null
       this.step++
+      console.log('preview',this.previews)
     },
     save(){
       this.inputImages.forEach(item=>{
@@ -189,13 +203,10 @@ export default {
       setTimeout(()=>{
         this.$router.push('/')
       },3000)
-       
     },
     prev(){
       this.step--
     },
   }
-
 }
 </script>
-
